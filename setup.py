293fafb1affd8e04c194b23dc2a921e96db8ec6c
YasinShaikh2003 <157198842+YasinShaikh2003@users.noreply.github.com>
2026-04-01 15:31:26 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="subspider",
+    version="1.0",
+    description="SubSpider - Passive Subdomain & IP Discovery Tool",
+    author="YourName",
+    py_modules=["subspider"],
+    install_requires=[
+        "aiohttp",
+        "requests",
+        "dnspython",
+        "colorama"
+    ],
+    entry_points={
+        "console_scripts": [
+            "subspider=subspider:main"
+        ]
+    }
+)
